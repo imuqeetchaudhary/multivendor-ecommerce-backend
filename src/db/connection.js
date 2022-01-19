9915aa37c9a1d2ba995dc071485d3209177261f5
Abdul Muqeet Arshad <imuqeetchaudhary@gmail.com>
2022-01-19 17:12:32 +0500

    changed mongodb uri

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const DEV_MONGO_URI = `mongodb://localhost:27017/node-rest-shop`;
 
-const PROD_MONGO_URI = `mongodb+srv://imuqeetchaudhary:${process.env.MONGO_DB_PW}@cluster0.bqu75.mongodb.net/node-rest-shop`;
+const PROD_MONGO_URI = `mongodb+srv://${process.env.MONGO_DB_UN}:${process.env.MONGO_DB_PW}@cluster0.bqu75.mongodb.net/online-shop`;
 
 const MONGO_URI =
   process.env.NODE_ENV === "production" ? PROD_MONGO_URI : DEV_MONGO_URI;
