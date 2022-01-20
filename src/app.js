a25fa3702ea889d3f2f82ac1dd3b2de978d98d0b
Abdul Muqeet Arshad <imuqeetchaudhary@gmail.com>
2022-01-20 18:35:11 +0500

    change the position of morgan to work it properly

--- src/app.js
+++ src/app.js
@@ -9,15 +9,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors({ origin: "*" }));
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static(__dirname + "/../uploads"));
-
-app.use("/", allRoutes);
 
 if (app.get("env") === "development") {
   app.use(morgan("tiny"));
   console.log("Morgan enable...");
 }
+
+app.use("/", allRoutes);
 
 app.get("/", (req, res) => res.json({ message: "Online Shop RESTful API" }));
 
