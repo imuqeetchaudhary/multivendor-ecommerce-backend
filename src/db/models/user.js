3d361d15dd7b4d87044bc55c62abf78b45c95271
Abdul Muqeet Arshad <imuqeetchaudhary@gmail.com>
2022-01-24 20:46:24 +0500

    added validation for email

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -9,6 +9,10 @@ const userSchema = new Schema({
   email: {
     type: String,
     required: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please enter a valid email address",
+    ],
   },
   password: {
     type: String,
