eaa679a861eca3f7742e08876c23bc1248000c9a
Abdul Muqeet Arshad <imuqeetchaudhary@gmail.com>
2022-02-02 20:16:23 +0500

    added services for creating, getting, updating & deletig cart

--- src/models/cart.js
+++ src/models/cart.js
@@ -19,16 +19,17 @@ exports.init = (sequelize, DataTypes, { User, Product }) => {
       quantity: {
         type: DataTypes.INTEGER.UNSIGNED,
         allowNull: false,
+        defaultValue: 1,
       },
     },
     { underscored: true, tablename: "cart" }
   );
 
   User.hasMany(Cart, { foreignKey: "user_id" });
-  Cart.belongsTo(Cart, { foreignKey: "user_id" });
+  Cart.belongsTo(User, { foreignKey: "user_id" });
 
   Product.hasMany(Cart, { foreignKey: "product_id" });
-  Cart.belongsTo(Cart, { foreignKey: "product_id" });
+  Cart.belongsTo(Product, { foreignKey: "product_id" });
 
   return Cart;
 };
---
+++ src/services/cart.js
@@ -0,0 +1,72 @@
+const db = require("../models");
+
+exports.createCart = async ({ userId, productId }) => {
+  try {
+    const cart = await db.Cart.create({
+      userId,
+      productId,
+    });
+
+    return cart;
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+exports.getAllCarts = async ({ userId }) => {
+  try {
+    const carts = await db.Cart.findOne({
+      where: { userId },
+      attributes: [["cart_id", "_id"], "quantity"],
+      include: [
+        {
+          model: db.User,
+          attributes: [["user_id", "_id"], "name", "email"],
+        },
+        {
+          model: db.Product,
+          attributes: [
+            ["product_id", "_id"],
+            "title",
+            "description",
+            "price",
+            "image",
+          ],
+        },
+      ],
+    });
+    if (!carts) throw new Error("No product found in cart");
+    return carts;
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+exports.updateCart = async ({ id, productId, quantity }) => {
+  try {
+    const updateCart = await db.Cart.update(
+      {
+        productId,
+        quantity,
+      },
+      { where: { cartId: id } }
+    );
+
+    return updateCart == 0
+      ? "Cart product not found"
+      : "Successfully updated cart product";
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+exports.deleteCart = async ({ id }) => {
+  try {
+    const cart = await db.Cart.destroy({ where: { cartId: id } });
+    return cart == 1
+      ? "Successfully deleted cart product"
+      : "Cart product doesn't exixts";
+  } catch (err) {
+    throw new Error(err);
+  }
+};
