a90490cbb45642d8214e3d54ec02ca2bc4f1b160
Abdul Muqeet Arshad <imuqeetchaudhary@gmail.com>
2022-02-08 15:27:47 +0500

    changed table id's configurations

--- src/services/product.js
+++ src/services/product.js
@@ -1,93 +1,77 @@
-const db = require("../models");
+const db = require('../models');
 
 exports.createProduct = async ({ title, price, image, description }) => {
-  try {
-    const product = await db.Product.create({
-      title,
-      price,
-      image,
-      description,
-    });
+	try {
+		const product = await db.Product.create({
+			title,
+			price,
+			image,
+			description,
+		});
 
-    return product;
-  } catch (err) {
-    throw new Error(err);
-  }
+		return product;
+	} catch (err) {
+		throw new Error(err);
+	}
 };
 
 exports.getAllProducts = async () => {
-  try {
-    const products = await db.Product.findAll({
-      attributes: [
-        ["product_id", "_id"],
-        "title",
-        "description",
-        "price",
-        "image",
-      ],
-    });
-    return products;
-  } catch (err) {
-    throw new Error(err);
-  }
+	try {
+		const products = await db.Product.findAll({
+			attributes: ['productId', 'title', 'description', 'price', 'image'],
+		});
+		return products;
+	} catch (err) {
+		throw new Error(err);
+	}
 };
 
 exports.getSingleProduct = async ({ id }) => {
-  try {
-    const products = await db.Product.findByPk(id, {
-      attributes: [
-        ["product_id", "_id"],
-        "title",
-        "description",
-        "price",
-        "image",
-      ],
-    });
-    if (!products) return "Product not found";
-    return products;
-  } catch (err) {
-    throw new Error(err);
-  }
+	try {
+		const products = await db.Product.findByPk(id, {
+			attributes: ['productId', 'title', 'description', 'price', 'image'],
+		});
+		if (!products) return 'Product not found';
+		return products;
+	} catch (err) {
+		throw new Error(err);
+	}
 };
 
 exports.updateProduct = async ({ id, title, price, description }) => {
-  try {
-    const updateProduct = await db.Product.update(
-      {
-        title,
-        price,
-        description,
-      },
-      { where: { productId: id } }
-    );
+	try {
+		const updateProduct = await db.Product.update(
+			{
+				title,
+				price,
+				description,
+			},
+			{ where: { productId: id } }
+		);
 
-    console.log(updateProduct);
+		console.log(updateProduct);
 
-    return updateProduct == 0
-      ? "Product not found"
-      : "Successfully updated product";
-  } catch (err) {
-    throw new Error(err);
-  }
+		return updateProduct == 0 ? 'Product not found' : 'Successfully updated product';
+	} catch (err) {
+		throw new Error(err);
+	}
 };
 
 exports.deleteProduct = async ({ id }) => {
-  try {
-    const products = await db.Product.destroy({ where: { productId: id } });
+	try {
+		const products = await db.Product.destroy({ where: { productId: id } });
 
-    return products == 1
-      ? "Successfully deleted Product"
-      : "Product doesn't exixts";
-  } catch (err) {
-    throw new Error(err);
-  }
+		return products == 1 ? 'Successfully deleted Product' : "Product doesn't exixts";
+	} catch (err) {
+		throw new Error(err);
+	}
 };
 
 exports.excludeCreatedAtUpdatedAt = ({ product }) => {
-  try {
-    const { createdAt, updatedAt, ...rest } = product.dataValues;
-    return rest;
-  } catch (err) {
-    throw new Error(err);
-  }
+	try {
+		const { createdAt, updatedAt, ...rest } = product.dataValues;
+		return rest;
+	} catch (err) {
+		throw new Error(err);
+	}
 };
